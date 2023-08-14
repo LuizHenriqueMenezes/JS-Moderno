b84c080d9fde0416659fb7b98bb9e9371ab53a7f
Luiz Henrique <lmenezesmessias@gmail.com>
2023-08-14 10:01:42 -0300

    commonJS

---
+++ conteudo/modulos/commonJS/commonJS.js
@@ -0,0 +1,8 @@
+const render= require("./render.js")
+const store=require("./store.js")
+//isso aqui não é suportado nos navegadores
+
+console.log("Aplicação iniciada.")
+render()
+store()
+console.log("Aplicação finalizada.")
