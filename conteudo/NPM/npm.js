53434dee7b7d7fe60a0304e94cf985bf854c6718
Luiz Henrique <lmenezesmessias@gmail.com>
2023-08-21 09:44:06 -0300

    push leve

---
+++ conteudo/NPM/npm.js
@@ -0,0 +1,8 @@
+// pra utilizar um módulo que eu instalei:
+const lodash = require("lodash")  
+
+//exemplos de como usar:
+
+console.log(lodash.isArray([])) // esse comando confere se é um array
+
+console.log(lodash.kebabCase("Luiz Henrique")) // esse comando separa as coisas por tracinho 
